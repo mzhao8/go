fa92d198f0a6057b505a1ee7a02f0651834da929
mzhao8 <mzhao8@gmail.com>
2021-01-01 21:10:56 -0500

    go bot first commit

---
+++ dlgo/agent/base.py
@@ -0,0 +1,9 @@
+""" all this bot needs to do is select a move given the current game state"""
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_move(self, game_state):
+        raise NotImplementedError()
